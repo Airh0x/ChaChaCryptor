46b5ce89679e48c052683b6f8a8b12d0026904d9
februarysn0w <repenwanker@februarysnow.com>
2025-07-17 17:12:58 +0900

    Initial Commit

---
+++ chachaCrypto/chachaCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  chachaCryptoApp.swift
+//  chachaCrypto
+//
+//  Created by KarUpas on 2025/07/17.
+//
+
+import SwiftUI
+
+@main
+struct chachaCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
